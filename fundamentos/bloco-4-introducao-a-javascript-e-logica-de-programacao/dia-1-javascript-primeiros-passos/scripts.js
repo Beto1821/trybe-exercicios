4698b1407f704c9264f72da245af85c0e15ff60e
Beto1821 <beto1821@uol.com.br>
2022-03-07 19:17:51 -0300

    Exercicios de 1 a 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,50 @@
+//Exercicio 1 
+// let a = 20;
+// let b = 5;
+
+// let soma = a + b;
+// console.log(soma);
+
+// let sub = a - b;
+// console.log(sub);
+
+// let mult = a * b;
+// console.log(mult);
+
+// let div = a / b;
+// console.log(div);
+
+// let modulo = a % b;
+// console.log(modulo);
+
+//Exercicio 2
+
+// let a = 10;
+// let b = 30;
+
+// if (a > b) {
+//     console.log("O valor A é Maior que o B");
+// }
+// else {
+//     console.log("O valor de B é maior que o A");
+// }
+
+//Exercico 3
+
+// let a = 60;
+// let b = 72;
+// let c = 55;
+
+// if (a > b && a > c) {
+//     console.log("O numero de A é o maior");
+// }
+
+// else if (b > a && b > c) {
+//     console.log("O numero de B é o maior");
+// }
+
+// else {
+//     console.log("O numero de C é o maior");
+// }
+
+//Exercicio 4
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/variaves.js
+++
@@ -1,5 +0,0 @@
-let myName = "Adalberto";
-let birthCity = "São Gonçalo do Sapucaí";
-let birthYear = 1975;
-
-console.log(myName, birthCity, birthYear);
