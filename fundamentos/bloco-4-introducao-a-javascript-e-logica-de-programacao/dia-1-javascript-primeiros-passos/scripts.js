e4bf14a4ba721b8560f11a2559b80f5fee23285b
Beto1821 <beto1821@uol.com.br>
2022-03-07 21:24:48 -0300

    exercicios de 1 a 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -23,10 +23,10 @@
 // let b = 30;
 
 // if (a > b) {
-//     console.log("O valor A é Maior que o B");
+//     console.log("O valor A é Maior que o B.");
 // }
 // else {
-//     console.log("O valor de B é maior que o A");
+//     console.log("O valor de B é maior que o A.");
 // }
 
 //Exercico 3
@@ -36,15 +36,70 @@
 // let c = 55;
 
 // if (a > b && a > c) {
-//     console.log("O numero de A é o maior");
+//     console.log("O numero de A é o maior.");
 // }
 
 // else if (b > a && b > c) {
-//     console.log("O numero de B é o maior");
+//     console.log("O numero de B é o maior.");
 // }
 
 // else {
-//     console.log("O numero de C é o maior");
+//     console.log("O numero de C é o maior.");
 // }
 
 //Exercicio 4
+
+// let valor = -1;
+
+// if (valor < 0) {
+//     console.log("Valor negativo.");
+// }
+// else if (valor > 0) {
+//     console.log("Valor positivo.");
+// }
+// else {
+//     console.log("Valor igual a zero.");
+// }
+
+//Exercicio 5
+
+// let ladoA = 45;
+// let ladoB = 75;
+// let ladoC = 65;
+// soma = ladoA + ladoB + ladoC
+
+// if (ladoA < 0 || ladoB < 0 || ladoC < 0) {
+//     console.log("ERRO");
+// }
+
+// else if (soma >= 0  && soma < 180 || soma > 180 ) {
+//     console.log("False");
+// }
+// else if (soma === 180) {
+//     console.log("True");
+// }
+
+//Exercicio 6
+
+// let peca = "Peão".toLowerCase();
+
+// if (peca === "peão" ) {
+//     console.log("Movimenta para frente e diagonal");
+// }
+// else if (peca === "torre") {
+//     console.log("Movimento para frente e para o lado");
+// }
+// else if (peca === "cavalo") {
+//     console.log("O cavalo move-se por duas casas horizontalmente ou verticalmente e então uma casa a mais em uma ângulo reto. O movimento do cavalo forma um “L”. ");
+// }
+// else if (peca === "rainha") {
+//     console.log("Ela pode mover-se qualquer número de casas em linha reta - verticalmente, horizontalmente ou diagonalmente. ");
+// }
+
+//Exercicio 7
+
+
+
+
+
+
