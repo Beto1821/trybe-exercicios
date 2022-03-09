6839aad39e180438c4faa9533a37e55b407a1c92
Beto1821 <beto1821@uol.com.br>
2022-03-09 16:16:31 -0300

    Exercicio 1 dia 4.3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -0,0 +1,9 @@
+//Exercicio 1
+let valor = 10;
+let num = 1;
+
+for (let i = valor; i > 0; i -= 1) {
+    num *= i;
+    console.log(num);
+}                                          
+console.log(num);
