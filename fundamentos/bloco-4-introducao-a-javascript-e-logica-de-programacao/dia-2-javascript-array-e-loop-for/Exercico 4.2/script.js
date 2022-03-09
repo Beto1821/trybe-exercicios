3748eba7d5a809d000aa98364215858b442c5557
Beto1821 <beto1821@uol.com.br>
2022-03-08 22:37:19 -0300

    exercicios 4.2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/Exercico 4.2/script.js
@@ -0,0 +1,68 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//Exercicio 1
+
+// for (let i = 0 ; i < numbers.length; i += 1) {
+//     console.log(numbers[i]);
+// }
+
+//exercicio 2
+
+// let soma = 0
+// for (let i = 0 ; i < numbers.length; i += 1) {
+//     soma = soma + numbers[i];
+// }
+// console.log(soma);
+
+//Exercicio 3
+
+// let soma = 0
+// let i = 0
+// for (i ; i < numbers.length; i += 1) {
+//     soma = soma + numbers[i];
+
+// }
+// let media = (soma/i)
+// console.log(media);
+
+//Exercicio 4
+
+// let soma = 0
+// let i = 0
+// for (i ; i < numbers.length; i += 1) {
+//     soma = soma + numbers[i];
+
+// }
+// let media = (soma/i)
+// console.log('A media dos numeros é :',media);
+
+// if (media > 20) {
+//     console.log("O valor maior que 20.");
+// }
+// else {
+//     console.log("O valor menor ou igual a 20.");
+// }
+
+//Exercicio 5
+
+// let maior = numbers[0];
+
+// for (i = 0; i < numbers.length; i += 1) {
+//     if (maior < numbers[i]) {
+//         maior = numbers[i];
+//     }
+// }
+// console.log('O maior número é ', maior);
+
+//Exercicio 6
+
+//Exercicio 8
+let limite = 25;
+
+let arrayNumeros = []
+
+for (let i = 1; i <= limite; i += 1) {
+    arrayNumeros.push(i)
+}
+
+console.log(arrayNumeros);
