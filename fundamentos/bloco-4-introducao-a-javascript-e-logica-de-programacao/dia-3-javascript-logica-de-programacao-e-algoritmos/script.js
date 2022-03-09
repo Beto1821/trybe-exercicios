41736b7db8abc326d8f3e7d3b98aeec98d6962bd
Beto1821 <beto1821@uol.com.br>
2022-03-09 19:31:39 -0300

    exercicio 2 /4.3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,9 +1,25 @@
 //Exercicio 1
-let valor = 10;
-let num = 1;
 
-for (let i = valor; i > 0; i -= 1) {
-  num *= i;
-  console.log(num);
-}
-console.log(num);
+// let valor = 10;
+// let num = 1;
+
+// for (let i = valor; i > 0; i -= 1) {
+//   num *= i;
+//   console.log(num);
+// }
+// console.log(num);
+
+//Exercicio 2
+
+// let word = 'tryber';
+// let revWorld = '';
+
+// for (let i = word.length - 1; i >= 0; i -= 1 ) {
+//     revWorld += word[i];
+// }
+
+// console.log(revWorld);
+
+//Exercicio 3
+
+
