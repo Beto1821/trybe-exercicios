d5f39229c6131c0cbff5e7b72ffb335eb2d832d7
Beto1821 <beto1821@uol.com.br>
2022-03-09 23:07:58 -0300

    exercicios 4.3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -22,4 +22,34 @@
 
 //Exercicio 3
 
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
 
+// let maior = '';
+// //let tamMaior = 0;
+// let menor = array[0];
+// //let tamMenor = array[0].length;
+
+// for (let i = 0; i < array.length; i += 1 ) {
+//     if (array[i].length > maior.length) {
+// //        tamMaior = array[i].length;
+//         maior = array[i];
+//     }
+//     if (array[i].length < menor.length) {
+// //        tamMenor = array[i].length;
+//         menor = array[i];
+//     }
+// }
+// console.log('A palavra de maior valor é :',maior);
+// console.log('A palavra de menor tamanho é :', menor);
+
+//Exercicio 4
+
+function primeNumber(num) {
+    for (let divisor = 2; divisor < num; divisor++) 
+    if (num % divisor == 0) return false;
+    return true;
+}
+
+var determinadoNumero = 50;
+
+for (let i = 2; i < determinadoNumero; i++) if (primeNumber(i)) console.log(i);
