6a247d62fc94ca3f024b569d9c4e7514b2af118b
Beto1821 <beto1821@uol.com.br>
2022-03-10 19:52:49 -0300

    para fixar objetos - 4.3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/aula/script.js
@@ -0,0 +1,26 @@
+// Para fixar
+
+let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };
+
+let player = {
+    name: 'Marta',
+    lastName : 'Silva',
+    age : 34,
+    medals : { golden: 2, silver: 3 },
+}
+
+
+
+//console.log('A jogadora', player.name, player.lastName,'tem', player.age,'anos de idade.');   
+
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+
+//console.log(player);
+
+console.log('A jogadora', player.name, player.lastName,'foi eleita a melhor do mundo', player.bestInTheWorld.length,'.');
+
+console.log('A jogadora possui', player.medals.golden ,'medalhas de Ouro e', player.medals.silver ,'medalhas de Prata.');
+
