dd443bb1cbd4655e19c3008311bc903827a77e81
Beto1821 <beto1821@uol.com.br>
2022-03-18 20:36:46 -0300

    querySelector

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/fixar/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/fixar/script.js
@@ -1,27 +1,25 @@
 const paragraph = document.getElementById("paragraph");
 paragraph.style.color = "red";
 
-
 // Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
-document.getElementById("page-title").innerText = 'Operação cavalo de Troia';
+document.querySelector("#page-title").innerText = "Operação cavalo de Troia";
 
 // Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
-document.getElementById('second-paragraph').innerText = 'Você vai desenvolver um site que contenha uma série de informações sobre o que você aprendeu aqui na Trybe ao longo dos últimos três blocos. Seu site deverá estar com elementos posicionados e estilizados e além disto, deverá conter semântica apropriada para que seja acessível e melhor ranqueado'
+document.getElementById("second-paragraph").innerText =
+  "Você vai desenvolver um site que contenha uma série de informações sobre o que você aprendeu aqui na Trybe ao longo dos últimos três blocos. Seu site deverá estar com elementos posicionados e estilizados e além disto, deverá conter semântica apropriada para que seja acessível e melhor ranqueado";
 
 // Por fim, recupere o subtítulo e altere-o também.
-document.getElementById('subtitle').innerText = 'Abobrinhas'
-document.getElementById('subtitle').style.color = 'green'
+document.getElementById("subtitle").innerText = "Abobrinhas";
+document.getElementById("subtitle").style.color = "green";
 
 // Adicione uma classe igual para os dois parágrafos.
 
-
 // Recupere os seus parágrafos via código JavaScript , usando a função getElementsByClassName ;
-document.getElementsByClassName('Parag');
+document.querySelector("p");
 
 // Altere algum estilo do primeiro deles.
-let Parag1 = document.getElementsByClassName('Parag')[0];
-Parag1.style.backgroundColor = 'pink'
-
+let Parag1 = document.querySelectorAll(".Parag")[0];
+Parag1.style.backgroundColor = "pink";
 
 // Recupere o subtítulo e altere a cor dele usando a função getElementsByTagName .
-document.getElementsByTagName('h4')[0].style.backgroundColor = 'yellow';
+document.getElementsByTagName("h4")[0].style.backgroundColor = "yellow";
