cab6e60eafe3107d1ca2b0e06fb0f22cd749aa10
Beto1821 <beto1821@uol.com.br>
2022-03-18 19:31:26 -0300

    getelementbyid fixar

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/fixar/script.js
@@ -0,0 +1,13 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+
+// Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
+document.getElementById("page-title").innerText = 'Operação cavalo de Troia';
+
+// Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
+document.getElementById('second-paragraph').innerText = 'Você vai desenvolver um site que contenha uma série de informações sobre o que você aprendeu aqui na Trybe ao longo dos últimos três blocos. Seu site deverá estar com elementos posicionados e estilizados e além disto, deverá conter semântica apropriada para que seja acessível e melhor ranqueado'
+
+// Por fim, recupere o subtítulo e altere-o também.
+document.getElementById('subtitle').innerText = 'Abobrinhas'
+document.getElementById('subtitle').style.color = 'green'
