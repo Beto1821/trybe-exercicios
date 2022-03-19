0675f67e8764b40a3163bc392da258a916689660
Beto1821 <beto1821@uol.com.br>
2022-03-18 21:41:59 -0300

    5.1 exercicios

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/fixar_2/script.js
@@ -0,0 +1,10 @@
+let titulo = document.getElementsByTagName('header');
+
+
+let titulosh3 = document.getElementsByTagName('h3');
+
+for (i in titulosh3) {
+    titulosh3[i].style.color = 'black';
+}
+
+
