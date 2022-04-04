7cdc84713c57e9d9a311a8258be1dc479cfff9a3
Beto1821 <beto1821@uol.com.br>
2022-04-04 10:25:11 -0300

    7.1 exercicios

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/ex02/script.js
@@ -0,0 +1,6 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2] ;
+
+const ordenado = (array) => {
+console.log(`Os números ${array.sort(function(a, b){return a - b})} se encontram ordenados de forma crescente!`)};
+
+ordenado(oddsAndEvens)
