c58b6bd68bf284a9e958da2c9fe547cd56b8bcfa
Beto1821 <beto1821@uol.com.br>
2022-04-05 21:49:50 -0300

    ex01 finalizado

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/sum.test.js
@@ -8,4 +8,16 @@ describe('Testes função sum', () => {
   test('verificar se resposta de soma(4,5) é igual a 9', () => {
     expect(sum(0, 0)).toEqual(0);
   })
+
+  test('verificar se resposta de soma(4,"5") gera erro', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrowError();
+  })
+
+  test('Parametros deve ser numeros', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrowError('parameters must be numbers');
+  })
 });
