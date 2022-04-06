1ab96f2e78b3d280be66fc9970d306b663be0d91
Beto1821 <beto1821@uol.com.br>
2022-04-06 11:22:19 -0300

    ex04 rev0 7.3

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/sum.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/sum.js
@@ -16,6 +16,40 @@ function myRemove(arr, item) {
   return newArr;
 }
 
-// implemente seus testes aqui
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
 
-module.exports = {sum, myRemove};
+function encode(frase) {
+  for (i of frase) {
+    frase = frase.replace('a', '1');
+    frase = frase.replace('e', '2');
+    frase = frase.replace('i', '3');
+    frase = frase.replace('o', '4');
+    frase = frase.replace('u', '5');
+  }
+  return frase;
+}
+
+function decode(frase) {
+  for (i in frase) {
+    frase = frase.replace('1', 'a');
+    frase = frase.replace('2', 'e');
+    frase = frase.replace('3', 'i');
+    frase = frase.replace('4', 'o');
+    frase = frase.replace('5', 'u');
+  }
+  return frase;
+}
+
+module.exports = {
+  sum, 
+  myRemove,
+  myFizzBuzz,
+  encode,
+  decode
+};
--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/sum.test.js
@@ -1,4 +1,4 @@
-const {sum, myRemove} = require('./sum.js');
+const {sum, myRemove, myFizzBuzz, encode, decode} = require('./sum.js');
 
 describe('Testes função sum', () => {
   test('verificar se resposta de soma(4,5) é igual a 9', () => {
@@ -38,4 +38,70 @@ describe('Testes função Myremove', () => {
     expect(myRemove(arr, 5)).not.toContain(5);
   })
 
+});
+
+describe('Teste função FizzBuzz', () => {
+  test('Teste se a função recebe o valor 15 e retorne "fizzbuzz"', () => {
+    expect(myFizzBuzz(15)).toEqual("fizzbuzz");
+  })
+
+  test('Teste se a função recebe o valor 9 e retorne "fizz"', () => {
+    expect(myFizzBuzz(9)).toEqual("fizz");
+  })
+
+  test('Teste se a função recebe o valor 10 e retorne "buzz"', () => {
+    expect(myFizzBuzz(10)).toEqual("buzz");
+  })
+
+  test('Teste se a função recebe um valor não divisivel nem por 3 e nem por 5 e retorne "fizzbuzz"', () => {
+    expect(myFizzBuzz(17)).toBe(17);
+  })
+
+  test('Teste se a função recebe o valor que não é um número e retorne "false"', () => {
+    expect(myFizzBuzz("a")).toEqual(false);
+  })
+});
+
+describe('Testes das funções encode e decode', () => {
+  test('Teste se encode é uma funções', () => {
+    expect(typeof encode).toEqual('function');
+  })
+
+  test('Teste se decode é uma funções', () => {
+    expect(typeof decode).toEqual('function');
+  })
+
+  test('Teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5 na função encode', () => {
+    const vogais = 'aeiou';
+    expect(encode(vogais)).toEqual('12345');
+  })
+
+  test('Teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5 na função encode', () => {
+    const vogais = 'aeiou';
+    expect(encode(vogais)).toEqual('12345');
+  })
+
+  test('Teste se os números 1, 2, 3, 4, 5 são convertidas nas vogais a, e, i, o, u na função encode', () => {
+    const num = '12345';
+    expect(decode(num)).toEqual('aeiou');
+  })
+  test('Teste se as letras não vogais são convertidas em 1, 2, 3, 4 e 5 na função encode', () => {
+    const vogais = 'bcdf';
+    expect(encode(vogais)).not.toContain('12345');
+  })
+
+  test('Teste se os números 6, 7, 8, 9 não são convertidas nas vogais a, e, i, o, u na função encode', () => {
+    const num = '6789';
+    expect(decode(num)).not.toContain('aeiou');
+  })
+  
+  test('Teste se a string que é retornada pelas funções têm o mesmo número de caracteres que a string passada como parâmetro', () => {
+    const vogais = 'aeiou';
+    expect(encode(vogais).length).toEqual(5);
+  })
+
+  test('Teste se a string que é retornada pelas funções têm o mesmo número de caracteres que a string passada como parâmetro', () => {
+    const num = '12345';
+    expect(decode(num).length).toEqual(5);
+  })
 });
