7e981e0cfdf1ad8dcc2ab66ade45b35c8d459b0f
Beto1821 <beto1821@uol.com.br>
2022-04-11 16:18:04 -0300

    exe06 8.2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios/script.js
@@ -77,15 +77,15 @@ const books = [
 // Dica: use a função forEach .
 // Copiar
 
-// function smallerName() {
-//   let nameBook = books[0].name
-//   // escreva aqui o seu código
-//   books.forEach((element) => nameBook = element.name.length < nameBook.length? element.name: nameBook);
-//   // Variável nameBook que receberá o valor do menor nome;
-//   return nameBook;
-// }
+function smallerName() {
+  let nameBook = books[0].name
+  // escreva aqui o seu código
+  books.forEach((element) => nameBook = element.name.length < nameBook.length ? element.name : nameBook);
+  // Variável nameBook que receberá o valor do menor nome;
+  return nameBook;
+}
 
-// console.log(smallerName());
+//console.log(smallerName());
 
 // 3 - Encontre o primeiro livro cujo nome possui 26 caracteres.
 // Copiar
@@ -108,5 +108,81 @@ function getNamedBook() {
 }
  
 
-console.log(getNamedBook());
+//console.log(getNamedBook());
 
+
+// 4 - Ordene os livros por data de lançamento em ordem decrescente.
+
+
+// const expectedResult = [
+//   {
+//     id: 1,
+//     name: 'As Crônicas de Gelo e Fogo',
+//     genre: 'Fantasia',
+//     author: { name: 'George R. R. Martin', birthYear: 1948 },
+//     releaseYear: 1991,
+//   },
+//   {
+//     id: 5,
+//     name: 'A Coisa',
+//     genre: 'Terror',
+//     author: { name: 'Stephen King', birthYear: 1947 },
+//     releaseYear: 1986,
+//   },
+//   {
+//     id: 4,
+//     name: 'Duna',
+//     genre: 'Ficção Científica',
+//     author: { name: 'Frank Herbert', birthYear: 1920 },
+//     releaseYear: 1965,
+//   },
+//   {
+//     id: 2,
+//     name: 'O Senhor dos Anéis',
+//     genre: 'Fantasia',
+//     author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+//     releaseYear: 1954,
+//   },
+//   {
+//     id: 3,
+//     name: 'Fundação',
+//     genre: 'Ficção Científica',
+//     author: { name: 'Isaac Asimov', birthYear: 1920 },
+//     releaseYear: 1951,
+//   },
+//   {
+//     id: 6,
+//     name: 'O Chamado de Cthulhu',
+//     genre: 'Terror',
+//     author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+//     releaseYear: 1928,
+//   },
+// ];
+
+// function booksOrderedByReleaseYearDesc() {
+//   return books.sort((livroA, livroB) => livroB.releaseYear - livroA.releaseYear)
+// }
+
+//console.log(booksOrderedByReleaseYearDesc());
+
+// 5 - Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário.
+// Copiar
+
+//const expectedResult = false;
+
+// function everyoneWasBornOnSecXX() {
+//   return books.every((livro) => (livro.author.birthYear > 1900 && livro.author.birthYear <= 2000));
+//   }; 
+  
+// console.log(everyoneWasBornOnSecXX()); 
+
+// 6 - Faça uma função que retorne true , se algum livro foi lançado na década de 80, e false , caso contrário.
+// Copiar
+
+//const expectedResult = true;
+
+function someBookWasReleaseOnThe80s() {
+  return books.some((livro) => livro.releaseYear >= 1980 && livro.releaseYear < 1990)
+}
+
+console.log(someBookWasReleaseOnThe80s());
