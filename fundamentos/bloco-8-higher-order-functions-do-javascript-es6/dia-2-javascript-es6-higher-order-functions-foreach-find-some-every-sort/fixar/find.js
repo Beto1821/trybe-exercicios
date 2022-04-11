3a7877feeaa3d88404491000325c63b0c32dbf81
Beto1821 <beto1821@uol.com.br>
2022-04-11 09:29:47 -0300

    fixar sort 8.2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixar/find.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixar/find.js
@@ -5,3 +5,27 @@ const findDivisibleBy3And5 = (number) => number % 3 === 0 && number % 5 === 0;
 console.log(numbers.find(findDivisibleBy3And5));
 
 //console.log(findDivisibleBy3And5());
+
+// 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+// Copiar
+// const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+// const findNameWithFiveLetters = () => {
+//   // Adicione seu código aqui:
+//   return names.find((name) => name.length === 5);
+// }
+
+// console.log(findNameWithFiveLetters());
+
+// 3 - Utilize o find para encontrar a música com id igual a 31031685 , caso ela exista:
+// Copiar
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+const findMusic =(id) => musicas.find((musica) => musica.id === id);
+
+
+console.log(findMusic('31031685'))
