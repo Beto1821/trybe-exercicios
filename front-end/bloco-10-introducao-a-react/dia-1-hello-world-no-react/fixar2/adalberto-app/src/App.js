e81977b7c5632877dc537fc0a99a2f559f4cc83a
Adalberto Ramos Ribeiro <beto1821@pop-os.localdomain>
2022-05-05 21:15:16 -0300

    fixacao

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/fixar2/adalberto-app/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import Nome from './Nome';
+
+function App() {
+  return <Nome /> ;
+}
+
+export default App;
---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/fixar2/adalberto-app/src/Nome.js
@@ -0,0 +1,10 @@
+import './Nome.css'
+import React from 'react';
+
+class Nome extends React.Component {
+  render() {
+    return <span className='nome'>Adalberto</span>
+  }
+}
+
+export default Nome;
