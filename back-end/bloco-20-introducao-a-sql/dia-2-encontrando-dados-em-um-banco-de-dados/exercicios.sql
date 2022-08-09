e8418e2eed5e31a7e35e649f82d436502af24ed2
beto1821 <beto1821@uol.com.br>
2022-08-09 19:58:30 -0300

    exerc. 20.2

---
+++ back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/exercicios.sql
@@ -0,0 +1,75 @@
+-- Active: 1660079668896@@127.0.0.1@3306@Scientists
+# Exercício 1 Escreva uma query para exibir a string "This is SQL Exercise, Practice and Solution".
+
+USE Scientists;
+SELECT 'this is SQL Exercise, Pratice and Solution';
+
+# Exercício 2 Escreva uma query para exibir três números em três colunas.
+
+SELECT 1, 2, 3;
+
+# Exercício 3 Escreva uma query para exibir a soma dos números 10 e 15.
+
+SELECT (3 * 4) + 12;
+
+# Exercício 5 Escreva uma query para exibir todas as informações de todos os cientistas.
+
+SELECT * FROM Scientists;
+
+# Exercício 6 Escreva uma query para exibir o nome como "nome do projeto" e as horas como "tempo de trabalho" de cada projeto.
+
+SELECT name As "nome do projeto" , hours AS "tempo de trabalho"  FROM Projects;
+
+# Exercício 7 Escreva uma query para exibir o nome dos cientistas em ordem alfabética.
+
+SELECT name FROM Scientists
+ORDER BY name;
+
+# Exercício 8 Escreva uma query para exibir o nome dos Projetos em ordem alfabética descendente.
+
+SELECT name FROM Projects
+ORDER BY name DESC;
+
+# Exercício 9 Escreva uma query que exiba a string "O projeto name precisou de hours horas para ser concluído." para cada projeto.
+
+SELECT CONCAT('O projeto ', name, ' precisou de ', hours, ' horas para ser concluido. ') AS resultado FROM Projects;
+
+# Exercício 10 Escreva uma query para exibir o nome e as horas dos três projetos com a maior quantidade de horas.
+
+SELECT name, hours FROM Projects
+ORDER BY hours DESC LIMIT 3;
+
+# Exercício 11 Escreva uma query para exibir o código de todos os projetos da tabela AssignedTo sem que haja repetições.
+
+SELECT DISTINCT project FROM AssignedTo;
+
+# Exercício 12 Escreva uma query para exibir o nome do  projeto com maior quantidade de horas.
+
+SELECT name FROM Projects 
+ORDER BY hours DESC 
+LIMIT 1;
+
+# Exercício 13 Escreva uma query para exibir o nome do segundo projeto com menor quantidade de horas.
+
+SELECT name FROM Projects 
+ORDER BY hours ASC 
+LIMIT 1 
+OFFSET 1;
+
+
+# Exercício 14 Escreva uma query para exibir todas as informações dos cinco projetos com a menor quantidade de horas.
+
+SELECT * FROM Projects 
+ORDER BY hours ASC 
+LIMIT 5;
+
+#Exercício 15 Escreva uma query que exiba a string "Existem Number cientistas na tabela Scientists.",
+#em que Number se refira a quantidade de cientistas.
+
+SELECT
+    CONCAT(
+        'Existem ',
+        COUNT(name),
+        ' cientistas na tabela Scientists.'
+    ) AS resultado
+FROM Scientists;
