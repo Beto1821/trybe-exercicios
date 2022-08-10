293997317c1d5012490cf6f5789ffa4213e6796a
beto1821 <beto1821@uol.com.br>
2022-08-09 21:37:16 -0300

    dump

---
+++ back-end/bloco-20-introducao-a-sql/dia-4-Manipulando tabelas/exercicios.sql
@@ -0,0 +1,44 @@
+-- Active: 1660079668896@@127.0.0.1@3306@Pixar
+-- Exercício 1 Insira os filmes da Pixar abaixo na tabela Movies: Monstros SA,
+-- de Pete Docter,
+-- lançado em 2001,
+-- com 92 minutos de duração.Procurando Nemo,
+-- de John Lasseter,
+-- lançado em 2003,
+-- com 107 minutos de duração.Os Incríveis,
+-- de Brad Bird,
+-- lançado em 2004,
+-- com 116 minutos de duração.WALL - E,
+-- de Pete Docter,
+-- lançada em 2008,
+-- com 104 minutos de duração.
+
+INSERT INTO Movies(title, director, year, length_minutes)
+  VALUES ('Monstros SA', 'Pete Docter', 2001, 92 ),
+         ('Procurando Nemo', 'Jon Lasseter', 2003, 107), 
+         ('Os Incríveis', 'Brad Bird', 2004, 116),
+         ('WALL-E', 'Pete Docter', 2008, 104);
+
+         #Exercício 2
+#Procurando Nemo foi aclamado pela crítica! Foi classificado em 6.8, fez 450 milhões no mercado interno e 370 milhões no mercado internacional. Adicione as informações à tabela BoxOffice.
+
+INSERT INTO BoxOffice(movie_id, rating, domestic_sales, international_sales )
+VALUE (8, 6.8, 450000000, 370000000);
+
+# Exercício 3 O diretor do filme "Procurando Nemo" está incorreto,na verdade ele foi dirigido por "Andrew Staton".Corrija esse dado utilizando o UPDATE.
+
+UPDATE Movies
+SET director = "Andrew Staton"
+WHERE title = "Procurando Nemo"; 
+
+SELECT * FROM Pixar.Movies;
+
+-- Exercício 4 O título do
+--     filme "Ratatouille" está escrito de forma incorreta na tabela Movies,
+--     além disso,
+--     o filme foi lançado em 2007 e não em 2010. Corrija esses dados utilizando o
+-- UPDATE.
+
+UPDATE Movies
+SET title = "Ratatouille" , year = 2007
+WHERE title = 'ratatui'
