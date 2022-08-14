3e154818e657451ac7ff600c37e1adf85fc44b89
beto1821 <beto1821@uol.com.br>
2022-08-14 17:03:26 -0300

    21.2 fixar 1 exe2

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/fixar1.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/fixar1.sql
@@ -1,9 +1,19 @@
 -- Active: 1660079668896@@127.0.0.1@3306@sakila
 
-# 1 ADD
+# 1 
 SELECT
     a.actor_id,
-    a.first_name,
+    CONCAT(a.first_name, ' ', a.last_name) AS name,
     fa.film_id
 FROM actor AS a
     INNER JOIN film_actor AS fa ON a.actor_id = fa.actor_id;
+
+# 2
+SELECT
+    s.first_name,
+    s.last_name,
+    a.address
+FROM 
+    sakila.staff AS s
+INNER JOIN 
+    sakila.address AS a ON s.address_id = a.address_id;
