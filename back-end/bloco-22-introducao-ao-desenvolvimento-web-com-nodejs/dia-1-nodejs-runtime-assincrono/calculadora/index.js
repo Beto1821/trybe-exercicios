dbd39bdbf1ea6e3219feb5e8119d291122235f78
beto1821 <beto1821@uol.com.br>
2022-08-19 15:28:06 -0300

    calculadora npm

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/calculadora/index.js
@@ -0,0 +1,25 @@
+const operations = require('./operacoes')
+const readLine = require('readline-sync')
+
+console.log("Calculadora Simples")
+const num1 = readLine.questionInt("Digite um número: ")
+const oper = readLine.question("Digite a operacoes: (+, -, *, /)")
+const num2 = readLine.questionInt("Digite um número: ")
+
+switch (oper) {
+    case "+":
+        operations.sum(num1, num2)
+        break;
+    case "-":
+        operations.sub(num1, num2)
+        break;
+    case "*":
+        operations.mul(num1, num2)
+        break; 
+    case "/":
+        operations.div(num1, num2)
+        break;
+    default:
+        console.log("Opa! A operação é desconocida ou não foi implementada")                              
+
+}
