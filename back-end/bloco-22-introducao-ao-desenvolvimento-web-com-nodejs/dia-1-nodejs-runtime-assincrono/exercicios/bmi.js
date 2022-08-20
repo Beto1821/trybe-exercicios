e01da5f59d332b25872bd16dfe2d6073374406ce
beto1821 <beto1821@uol.com.br>
2022-08-20 16:13:49 -0300

    22.1 exercicos IMC

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/exercicios/bmi.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/exercicios/bmi.js
@@ -7,5 +7,29 @@ const altura = readLine.questionInt("Digite altura(m): ")
 
 const resp = peso/(altura/100)**2
 
+    if (resp < 18.5) {
+      console.log('Abaixo do peso (magreza)');
+    }
+
+    if (resp >= 18.5 && resp < 25) {
+      console.log('Peso normal');
+    }
+
+    if (resp >= 25 && resp < 30) {
+      console.log('Acima do peso (sobrepeso)');
+    }
+
+    if (resp >= 30 && resp < 35) {
+      console.log('Obesidade grau I');
+    }
+
+    if (resp >= 35 && resp < 40) {
+      console.log('Obesidade grau II');
+    }
+
+    if (resp > 40) {
+      console.log('Obesidade grau III e IV');
+    } 
+
         console.log(`Seu indice IMC é de : ${resp.toFixed(2)}`);
         
